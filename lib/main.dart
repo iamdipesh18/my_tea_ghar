3e14abd02718f52181d1fb544d2f78c207dcda63
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-06-13 12:57:03 +0545

    provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:my_tea_ghar/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:my_tea_ghar/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:my_tea_ghar/models/user.dart';
 
-void main() async{
-    WidgetsFlutterBinding.ensureInitialized(); // Required before Firebase init
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Required before Firebase init
   await Firebase.initializeApp(); // Initialize Firebase
   runApp(MyApp());
 }
@@ -13,6 +16,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Wrapper());
+    return StreamProvider<MyUser?>.value(
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(home: Wrapper()),
+    );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_tea_ghar/models/user.dart';
 import 'package:my_tea_ghar/screens/authenticate/authenticate.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<MyUser>(context);
+    print(user);
     // We need to return either Home or Authentication Widget
     // This screen is like a point from where if user is authenticated then will go to home else go to authenticate
 
