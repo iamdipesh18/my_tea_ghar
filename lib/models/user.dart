1dd8d842041c15292643007e7f72488207285008
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-06-18 12:14:33 +0545

    building a user data model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,3 +6,17 @@ class MyUser {
   @override
   String toString() => 'MyUser(uid:$uid)';
 }
+
+class UserData {
+  final String uid;
+  final String name;
+  final String sugars;
+  final String strength;
+
+  UserData({
+    required this.uid,
+    required this.name,
+    required this.sugars,
+    required this.strength,
+  });
+}
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:my_tea_ghar/models/brew.dart';
+import 'package:my_tea_ghar/models/user.dart';
 
 class DatabaseService {
   final String uid;
@@ -17,22 +18,37 @@ class DatabaseService {
     });
   }
 
-// Convert snapshot to list of Brew
-List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
-  return snapshot.docs.map((doc) {
-    // Make sure to use `doc.data()` and cast as Map
-    final data = doc.data() as Map<String, dynamic>;
-    return Brew(
+  // Convert snapshot to list of Brew
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      // Make sure to use `doc.data()` and cast as Map
+      final data = doc.data() as Map<String, dynamic>;
+      return Brew(
+        name: data['name'] ?? '',
+        strength: data['strength'] ?? 0,
+        sugars: data['sugars'] ?? '0',
+      );
+    }).toList();
+  }
+
+  //user data from sanpshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    final data = snapshot.data() as Map<String, dynamic>;
+    return UserData(
+      uid: uid,
       name: data['name'] ?? '',
-      strength: data['strength'] ?? 0,
       sugars: data['sugars'] ?? '0',
+      strength: data['strength'] ?? 0,
     );
-  }).toList();
-}
+  }
 
-// Get Brews Stream
-Stream<List<Brew>> get brews {
-  return brewCollection.snapshots().map(_brewListFromSnapshot);
-}
+  // Get Brews Stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
 
+  //get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
+  }
 }
